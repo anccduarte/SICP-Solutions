7b2ec48ebe1f2520c5fed4e20ad2fc8321c24424
anccduarte <antonionuno.123@gmail.com>
2024-06-05 13:54:50 +0100

    Reinforced solution for the halting problem

--- solutions/chapter-4/exercise-4-15.rkt
+++ solutions/chapter-4/exercise-4-15.rkt
@@ -40,9 +40,43 @@
 ;Now, suppose that we run 'D' having <D> has its input. In such a circumstance, we get
 ;the following behavior
 ;---
-;D(<p>) = | accept if 'D' does not accept <D>
+;D(<D>) = | accept if 'D' does not accept <D>
 ;         | reject if 'D' accepts <D>
 ;---
 ;"No matter what 'D' does, it is forced to do the opposite, which obviously constitutes a
 ;contradiction. Thus, neither 'D' nor 'H' can exist."
+
+;Embodying 'H' and 'D'
+;[note that two versions of 'D' are provided - see discussion in next section]
+;---
+(define (H p a) 'assume-existence)
+;---
+(define (D1 p)
+  (if (eq? (H p p) 'accept)
+      'reject
+      'accept))
+;---
+(define (D2 p a)
+  (if (eq? (H p a) 'accept)
+      'reject
+      'accept))
+
+;Discussing the behavior of 'D'
+;---
+;As previously mentioned, 'D1' sets the logical bind by subverting the outcome of 'P'.
+;Calling (D1 D1) yields 'accept if (D1 D1) results in 'reject and yields 'reject if the
+;outcome of (D1 D1) is 'accept. This obviously constitutes a contradiction. The reason
+;for calling 'D1' on 'D1' is a bit hermetic at first glance. Suppose that instead of
+;calling 'D1' on 'D1' itself, we runned it on a putative procedure 'R'. Then, executing
+;(D1 R) returns 'accept if 'R' rejects itself and 'reject if 'R' accepts itself. This
+;does not constitute a contradiction, and thus the reason for the self-referential call.
+;---
+;The reason for materializing 'D2' is an attempt to answer the question "Why does 'D' has
+;to be a one-argument procedure?". 'H' applies (somewhere in its body definition) 'P' to
+;'A' (remember that 'P' must, in this case, be a one-argument procedure). Defining 'D' as
+;a procedure of two arguments is ruinous, since it would "furtively" result in an error
+;when running (D2 D2 <arg>). The error would be thrown at the time 'H' is executed with
+;arguments 'D2' and <arg> since 'H' is to make the call (D2 <arg>) in its inner workings,
+;resulting in a mismatch in the number of formal parameters and arguments of 'D2'. Hence,
+;'D' must imperatively be a single-argument procedure.
 
